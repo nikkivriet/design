e4f5d9d6431f8d09884348b5694ef448c153ea6a
albin <albin_a@live.se>
2019-04-09 18:47:24 +0200

    filer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="app">
     <div id="nav">
       <!--<router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> -->
@@ -23,15 +23,29 @@
     <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
   </card>
 </div>
+<div id="app">
+
+    <button id="show-modal" @click="showModal = true">Show Modal</button>
+    <!-- use the modal component, pass in the prop -->
+    <modal v-if="showModal" @close="showModal = false">
+      <!--
+        you can use custom content here to overwrite
+        default content
+      -->
+      <h3 slot="header">custom header</h3>
+    </modal>
+  </div>
     </div>
    <!-- <router-view/> -->
   </div>
 </template>
 <script>
-import card from './components/Card1'
+import card from '@/components/Card1'
+import modal from '@/components/ModalWindow'
 export default {
   components: {
     card,
+    modal,
   },
   data () {
     return {
@@ -41,25 +55,12 @@ export default {
 }
 </script>
 
-<style>
+<style <style lang="scss">
 @import './assets/styles/card.scss';
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
+
+<style scoped>
+@import './assets/styles/googlefontcss.css';
+@import './assets/styles/modal.css';
+</style>
+
---
+++ src/components/ ModalWindow.vue
@@ -0,0 +1,53 @@
+<template> 
+<div> 
+    <transition name="modal">
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+
+          <div class="modal-header">
+            <slot name="header">
+              default header
+            </slot>
+          </div>
+
+          <div class="modal-body">
+            <slot name="body">
+              default body
+            </slot>
+          </div>
+
+          <div class="modal-footer">
+            <slot name="footer">
+              default footer
+              <button class="modal-default-button" @click="$emit('close')"> OK </button>
+            </slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'modal',
+  props: {
+    msg: String
+  },
+  data: {
+    showModal: false
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
+
+})
